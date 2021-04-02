be4a2ac620a10320a3f139495643a44f0502adb5
Edoardo Balzani <eb162@ad.nyu.edu>
2021-04-02 05:02:43 -0400

    lfp updated code

--- analyzing/lfp_crossArea/cross_coherence_funcs.py
+++ analyzing/lfp_crossArea/cross_coherence_funcs.py
@@ -7,12 +7,7 @@ import nitime.algorithms as tsa
 from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
 
-font = {'family' : 'arial',
-        'weight' : 'normal',
-        'size'   : 16}
 
-plt.rc('font', **font)
-plt.rc({'axes.labelsize', 'medium'})
 
 
 def dB(x, out=None):
@@ -38,8 +33,8 @@ def mtem(i, j, dt):
     ph     phase spectrum between ij at input freq
 
     """
-    print('i size', i.shape)
-    print('j size', j.shape)
+    # print('i size', i.shape)
+    # print('j size', j.shape)
 
     # apply multi taper cross spectral density from nitime module
     f, pcsd_est = tsa.multi_taper_csd(np.vstack([i, j]), Fs=1 / dt, low_bias=True, adaptive=True, sides='onesided')
@@ -109,156 +104,162 @@ def mtem_unct(i_, j_, dt_, cf, mc_no=20):
     phif = phi[1:-1]
     return phif
 
-
-end = 2000
-dt=1
-t = np.arange(0,end,dt)
-rand1 = np.random.rand(end)
-rand2 = np.random.rand(end)
-i = 1*np.cos(2*np.pi*90/360+2*np.pi/21*t)+4*np.cos(+2*np.pi*10/360-2*np.pi/10*t)+1.5*(-1+2*rand1) # two freq
-
-# for j prepared two functions, one time series that contain two freqs and one that contain one freq
-j = 1*np.cos(2*np.pi*60/360+2*np.pi/21*t)+4*np.cos(-2*np.pi*70/360-2*np.pi/10*t)+1.5*(-1+2*rand2) # two freq
-#j = 4*np.cos(-2*np.pi*70/360-2*np.pi/10*t)+1.5*(-1+2*rand2) # one freq
-
-plt.figure(figsize=(9,4))
-plt.subplot(111)
-plt.grid()
-
-plt.plot(t,i, 'r', lw=1, label='i')
-plt.plot(t,j, 'b', lw=1, label='j')
-plt.xlim(0,100)
-
-plt.ylabel('amplitude')
-plt.xlabel('time (hours)')
-lg = plt.legend()
-lg.get_frame().set_ec('lightgray')
-lg.get_frame().set_lw(0.5)
-
-plt.gcf().tight_layout()
-
-f, fki, fkj, cij, ph, coh = mtem(i,j,dt)
-
-
-vspan_start1 = 0.0465
-vspan_end1 = 0.0485
-vspan_start2 = 0.099
-vspan_end2 = 0.101
-xlim_start = 0.000
-xlim_end = 0.25
-
-
-
-plt.figure(figsize=(9,4))
-ax = host_subplot(111, axes_class=AA.Axes)
-
-ax.set_xlim([xlim_start,xlim_end])
-ax.set_xlabel('frequency (Hz)')
-ax.set_ylabel('phase (day)')
-ax.set_title('t (hours)', loc='left')
-
-ax2 = ax.twin() # ax2 is responsible for "top" axis and "right" axis
-ax2.set_xticks([(0.05),(0.1),(0.15),(0.2), (0.25)])
-ax2.set_xticklabels([str(1./0.05),str(1./0.1),str(round(1./0.15,1)),str(1./0.2),str(1./0.25)])
-ax2.axis["right"].major_ticklabels.set_visible(False)
-
-ax.grid(axis='y')
-ax.plot(f,dB(fki), 'r-', lw=1, label ='psd i')
-ax.plot(f,dB(fkj), 'b-', lw=1, label='psd j')
-plt.axvspan(vspan_start1,vspan_end1, color='gray', alpha=0.35)
-plt.axvspan(vspan_start2,vspan_end2, color='gray', alpha=0.35)
-plt.ylim(-10,40)
-
-ax.set_ylabel('power (10log10)') # regex: ($10log10$)
-ax.set_xlabel('frequency (Hz)')
-lg = plt.legend()
-lg.get_frame().set_ec('lightgray')
-lg.get_frame().set_lw(0.5)
-
-plt.gcf().tight_layout()
-
-
-
-
-plt.figure(figsize=(9,4))
-ax = host_subplot(111, axes_class=AA.Axes)
-
-ax.set_xlim([xlim_start,xlim_end])
-ax.set_xlabel('frequency (Hz)')
-ax.set_ylabel('phase (day)')
-ax.set_title('t (hours)', loc='left')
-
-ax2 = ax.twin() # ax2 is responsible for "top" axis and "right" axis
-ax2.set_xticks([(0.05),(0.1),(0.15),(0.2), (0.25)])
-ax2.set_xticklabels([str(1./0.05),str(1./0.1),str(round(1./0.15,1)),str(1./0.2),str(1./0.25)])
-ax2.axis["right"].major_ticklabels.set_visible(False)
-
-ax.grid(axis='y')
-ax.plot(f,dB(cij), 'g-', lw=1, label='csd ij')
-plt.axvspan(vspan_start1,vspan_end1, color='gray', alpha=0.35)
-plt.axvspan(vspan_start2,vspan_end2, color='gray', alpha=0.35)
-plt.ylim(-10,40)
-
-ax.set_ylabel('cross-power (10log10)') # regex: ($10log10$)
-ax.set_xlabel('frequency (Hz)')
-plt.gcf().tight_layout()
-
-
-
-plt.figure(figsize=(9,4))
-ax = host_subplot(111, axes_class=AA.Axes)
-
-ax.set_xlim([xlim_start,xlim_end])
-ax.set_xlabel('frequency (Hz)')
-ax.set_ylabel('phase (day)')
-ax.set_title('t (hours)', loc='left')
-
-ax2 = ax.twin() # ax2 is responsible for "top" axis and "right" axis
-ax2.set_xticks([(0.05),(0.1),(0.15),(0.2), (0.25)])
-ax2.set_xticklabels([str(1./0.05),str(1./0.1),str(round(1./0.15,1)),str(1./0.2),str(1./0.25)])
-ax2.axis["right"].major_ticklabels.set_visible(False)
-
-# plt.subplot(122)
-ax.grid(axis='y')
-ax.set_ylim([0,1.01])
-ax.plot(f,coh, 'darkorange', label='coherence ij')
-
-plt.axvspan(vspan_start1,vspan_end1, color='gray', alpha=0.35)
-plt.axvspan(vspan_start2,vspan_end2, color='gray', alpha=0.35)
-ax.set_xlim(xlim_start,xlim_end)
-
-ax.set_ylabel('coherence')
-ax.set_xlabel('frequency (Hz)')
-
-plt.gcf().tight_layout()
-
-
-
-phif = mtem_unct(i,j,dt,coh, mc_no=15)
-
-plt.figure(figsize=(9,4))
-ax = host_subplot(111, axes_class=AA.Axes)
-
-ax.set_xlim([xlim_start,xlim_end])
-ax.set_yticks([0,1./4*np.pi, np.pi/2, 3./4*np.pi,np.pi])
-ax.set_yticklabels([r'$0$', r'$\frac{1}{4}\pi$', r'$\frac{\pi}{2}$', r'$\frac{3}{4}\pi$', r'$\pi$'])
-
-ax.set_ylabel('phase uncertainty (radian)')
-ax.set_xlabel('frequency (Hz)')
-ax.set_title('t (hours)', loc='left')
-
-ax2 = ax.twin() # ax2 is responsible for "top" axis and "right" axis
-ax2.set_xticks([(0.05),(0.1),(0.15),(0.2), (0.25)])
-ax2.set_xticklabels([str(1./0.05),str(1./0.1),str(round(1./0.15,1)),str(1./0.2),str(1./0.25)])
-ax2.axis["right"].major_ticklabels.set_visible(False)
-ax.grid(axis='y')
-
-ax.plot(f,phif, 'c', lw=1, label='uncertainty')
-plt.axvspan(vspan_start1,vspan_end1, color='gray', alpha=0.35)
-plt.axvspan(vspan_start2,vspan_end2, color='gray', alpha=0.35)
-
-ax.set_xlim(xlim_start,xlim_end)
-ax.set_ylim(-0.05,3.2)
-
-plt.gcf().tight_layout()
-
+if __name__ == '__main__':
+    font = {'family' : 'arial',
+            'weight' : 'normal',
+            'size'   : 16}
+    
+    plt.rc('font', **font)
+    plt.rc({'axes.labelsize', 'medium'})
+    end = 2000
+    dt=1
+    t = np.arange(0,end,dt)
+    rand1 = np.random.rand(end)
+    rand2 = np.random.rand(end)
+    i = 1*np.cos(2*np.pi*90/360+2*np.pi/21*t)+4*np.cos(+2*np.pi*10/360-2*np.pi/10*t)+1.5*(-1+2*rand1) # two freq
+    
+    # for j prepared two functions, one time series that contain two freqs and one that contain one freq
+    j = 1*np.cos(2*np.pi*60/360+2*np.pi/21*t)+4*np.cos(-2*np.pi*70/360-2*np.pi/10*t)+1.5*(-1+2*rand2) # two freq
+    #j = 4*np.cos(-2*np.pi*70/360-2*np.pi/10*t)+1.5*(-1+2*rand2) # one freq
+    
+    plt.figure(figsize=(9,4))
+    plt.subplot(111)
+    plt.grid()
+    
+    plt.plot(t,i, 'r', lw=1, label='i')
+    plt.plot(t,j, 'b', lw=1, label='j')
+    plt.xlim(0,100)
+    
+    plt.ylabel('amplitude')
+    plt.xlabel('time (hours)')
+    lg = plt.legend()
+    lg.get_frame().set_ec('lightgray')
+    lg.get_frame().set_lw(0.5)
+    
+    plt.gcf().tight_layout()
+    
+    f, fki, fkj, cij, ph, coh = mtem(i,j,dt)
+    
+    
+    vspan_start1 = 0.0465
+    vspan_end1 = 0.0485
+    vspan_start2 = 0.099
+    vspan_end2 = 0.101
+    xlim_start = 0.000
+    xlim_end = 0.25
+    
+    
+    
+    plt.figure(figsize=(9,4))
+    ax = host_subplot(111, axes_class=AA.Axes)
+    
+    ax.set_xlim([xlim_start,xlim_end])
+    ax.set_xlabel('frequency (Hz)')
+    ax.set_ylabel('phase (day)')
+    ax.set_title('t (hours)', loc='left')
+    
+    ax2 = ax.twin() # ax2 is responsible for "top" axis and "right" axis
+    ax2.set_xticks([(0.05),(0.1),(0.15),(0.2), (0.25)])
+    ax2.set_xticklabels([str(1./0.05),str(1./0.1),str(round(1./0.15,1)),str(1./0.2),str(1./0.25)])
+    ax2.axis["right"].major_ticklabels.set_visible(False)
+    
+    ax.grid(axis='y')
+    ax.plot(f,dB(fki), 'r-', lw=1, label ='psd i')
+    ax.plot(f,dB(fkj), 'b-', lw=1, label='psd j')
+    plt.axvspan(vspan_start1,vspan_end1, color='gray', alpha=0.35)
+    plt.axvspan(vspan_start2,vspan_end2, color='gray', alpha=0.35)
+    plt.ylim(-10,40)
+    
+    ax.set_ylabel('power (10log10)') # regex: ($10log10$)
+    ax.set_xlabel('frequency (Hz)')
+    lg = plt.legend()
+    lg.get_frame().set_ec('lightgray')
+    lg.get_frame().set_lw(0.5)
+    
+    plt.gcf().tight_layout()
+    
+    
+    
+    
+    plt.figure(figsize=(9,4))
+    ax = host_subplot(111, axes_class=AA.Axes)
+    
+    ax.set_xlim([xlim_start,xlim_end])
+    ax.set_xlabel('frequency (Hz)')
+    ax.set_ylabel('phase (day)')
+    ax.set_title('t (hours)', loc='left')
+    
+    ax2 = ax.twin() # ax2 is responsible for "top" axis and "right" axis
+    ax2.set_xticks([(0.05),(0.1),(0.15),(0.2), (0.25)])
+    ax2.set_xticklabels([str(1./0.05),str(1./0.1),str(round(1./0.15,1)),str(1./0.2),str(1./0.25)])
+    ax2.axis["right"].major_ticklabels.set_visible(False)
+    
+    ax.grid(axis='y')
+    ax.plot(f,dB(cij), 'g-', lw=1, label='csd ij')
+    plt.axvspan(vspan_start1,vspan_end1, color='gray', alpha=0.35)
+    plt.axvspan(vspan_start2,vspan_end2, color='gray', alpha=0.35)
+    plt.ylim(-10,40)
+    
+    ax.set_ylabel('cross-power (10log10)') # regex: ($10log10$)
+    ax.set_xlabel('frequency (Hz)')
+    plt.gcf().tight_layout()
+    
+    
+    
+    plt.figure(figsize=(9,4))
+    ax = host_subplot(111, axes_class=AA.Axes)
+    
+    ax.set_xlim([xlim_start,xlim_end])
+    ax.set_xlabel('frequency (Hz)')
+    ax.set_ylabel('phase (day)')
+    ax.set_title('t (hours)', loc='left')
+    
+    ax2 = ax.twin() # ax2 is responsible for "top" axis and "right" axis
+    ax2.set_xticks([(0.05),(0.1),(0.15),(0.2), (0.25)])
+    ax2.set_xticklabels([str(1./0.05),str(1./0.1),str(round(1./0.15,1)),str(1./0.2),str(1./0.25)])
+    ax2.axis["right"].major_ticklabels.set_visible(False)
+    
+    # plt.subplot(122)
+    ax.grid(axis='y')
+    ax.set_ylim([0,1.01])
+    ax.plot(f,coh, 'darkorange', label='coherence ij')
+    
+    plt.axvspan(vspan_start1,vspan_end1, color='gray', alpha=0.35)
+    plt.axvspan(vspan_start2,vspan_end2, color='gray', alpha=0.35)
+    ax.set_xlim(xlim_start,xlim_end)
+    
+    ax.set_ylabel('coherence')
+    ax.set_xlabel('frequency (Hz)')
+    
+    plt.gcf().tight_layout()
+    
+    
+    
+    phif = mtem_unct(i,j,dt,coh, mc_no=15)
+    
+    plt.figure(figsize=(9,4))
+    ax = host_subplot(111, axes_class=AA.Axes)
+    
+    ax.set_xlim([xlim_start,xlim_end])
+    ax.set_yticks([0,1./4*np.pi, np.pi/2, 3./4*np.pi,np.pi])
+    ax.set_yticklabels([r'$0$', r'$\frac{1}{4}\pi$', r'$\frac{\pi}{2}$', r'$\frac{3}{4}\pi$', r'$\pi$'])
+    
+    ax.set_ylabel('phase uncertainty (radian)')
+    ax.set_xlabel('frequency (Hz)')
+    ax.set_title('t (hours)', loc='left')
+    
+    ax2 = ax.twin() # ax2 is responsible for "top" axis and "right" axis
+    ax2.set_xticks([(0.05),(0.1),(0.15),(0.2), (0.25)])
+    ax2.set_xticklabels([str(1./0.05),str(1./0.1),str(round(1./0.15,1)),str(1./0.2),str(1./0.25)])
+    ax2.axis["right"].major_ticklabels.set_visible(False)
+    ax.grid(axis='y')
+    
+    ax.plot(f,phif, 'c', lw=1, label='uncertainty')
+    plt.axvspan(vspan_start1,vspan_end1, color='gray', alpha=0.35)
+    plt.axvspan(vspan_start2,vspan_end2, color='gray', alpha=0.35)
+    
+    ax.set_xlim(xlim_start,xlim_end)
+    ax.set_ylim(-0.05,3.2)
+    
+    plt.gcf().tight_layout()
+    
