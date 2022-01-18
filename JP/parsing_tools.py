994944dcb3b2b2db329322a6acd5a9a36ac0626d
edoardo <edoardo.balzani87@gmail.com>
2022-01-18 13:16:33 -0500

    added plot graph

--- JP/parsing_tools.py
+++ JP/parsing_tools.py
@@ -1,7 +1,35 @@
 import numpy as np
 from scipy.io import loadmat,matlab
+import pydot
 
+menu = {'dinner':
+            {'chicken':'good',
+             'beef':'average',
+             'vegetarian':{
+                   'tofu':'good',
+                   'salad':{
+                            'caeser':'bad',
+                            'italian':'average'}
+                   },
+             'pork':'bad'}
+        }
 
+def draw(parent_name, child_name):
+    edge = pydot.Edge(parent_name, child_name)
+    graph.add_edge(edge)
+
+def visit(node, parent=None):
+    for k,v in node.iteritems():
+        if isinstance(v, dict):
+            # We start with the root node whose parent is None
+            # we don't want to graph the None node
+            if parent:
+                draw(parent, k)
+            visit(v, k)
+        else:
+            draw(parent, k)
+            # drawing the label using a distinct name
+            draw(k, k+'_'+v)
 
 
 def unNest_mat_structure(unNest, struc, init = False):
