c819e2a167d272480d94840a324c262f748bd9c5
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-07-29 09:25:42 -0400

    started penalty utils

--- src/PGAM/penalty_utils.py
+++ src/PGAM/penalty_utils.py
@@ -1,5 +1,9 @@
+import timeit
+
 import jax
 import jax.numpy as jnp
+import nemos as nmo
+import numpy as np
 
 
 @jax.jit
@@ -21,3 +25,17 @@ def symmetric_sqrt(M):
     return Bx[:, ::-1].T
 
 
+def compute_energy_penalty(n_samples, knots, order):
+    if order < 3:
+        raise ValueError("A second derivative based penalty can be computed for `order >= 3`. "
+                         f"The provided order is {order} instead!")
+    samples = np.linspace(0, 1, n_samples)
+    eval_bas = nmo.basis.bspline(samples, knots, order, der=2, outer_ok=False)
+    indices = jnp.triu_indices(eval_bas.shape[1])
+    square_bas = eval_bas[:, indices[0]] * eval_bas[:, indices[1]]
+    dx = samples[1] - samples[0]
+    integr = jax.scipy.integrate.trapezoid(square_bas, dx=dx, axis=0)
+    energy_pen = jnp.zeros((eval_bas.shape[1], eval_bas.shape[1]))
+    energy_pen = energy_pen.at[indices].set(integr)
+    energy_pen = energy_pen + energy_pen.T - jnp.diag(energy_pen.diagonal())
+    return energy_pen
